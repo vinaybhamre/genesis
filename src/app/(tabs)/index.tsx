d50c4930a0a82b4c1fc4d5cc2d3553f31c3f0c13
vinay <vinay86bhamre@gmail.com>
2026-04-16 16:33:09 +0530

    feat: add pull-to-refresh with simulated loading delay and shuffled feed data

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,31 @@
 import FeedCard from "@/components/FeedCard";
 import { tweets } from "@/data/tweets";
+import { shuffleArray } from "@/lib/shuffle";
+import { useState } from "react";
 import { FlatList, Text, View } from "react-native";
 
 export default function App() {
+  const [refreshingData, setRefreshingData] = useState(false);
+  const [tweetsData, setTweetsData] = useState(tweets);
+  function handleRefresh() {
+    setRefreshingData(true);
+    setTweetsData((data) => shuffleArray(data));
+    setTimeout(() => setRefreshingData(false), 500);
+  }
+
   return (
     <View className="flex-1 bg-white">
-      <Text className="text-xl font-bold text-blue-500 text-center align-middle h-16">
+      <Text className="h-16 text-center align-middle text-xl font-bold text-blue-500">
         Home Feed
       </Text>
       <FlatList
-        data={tweets}
+        data={tweetsData}
         renderItem={({ item }) => <FeedCard {...item} />}
         ItemSeparatorComponent={() => <View className="h-2" />}
         keyExtractor={(item) => item.id}
         contentContainerClassName="pb-16"
+        refreshing={refreshingData}
+        onRefresh={handleRefresh}
       />
     </View>
   );
